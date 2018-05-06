1e39cbf536c7ba032f578883cb1f97c8788b6a7c
Zed <clement.francois@epitech.eu>
2018-05-06 16:29:11 +0200

    BinaryResource: ADD: Additional information

--- screen_fleet_admin/Models/BinaryResourceModel.cs
+++ screen_fleet_admin/Models/BinaryResourceModel.cs
@@ -4,7 +4,7 @@ namespace screen_fleet_admin.Models
 {
     public class BinaryResourceModel : ResourceModel
     {
-        [BsonElement("Image data")]
-        public byte[] Image { get; set; }
+        [BsonElement("Binary data")]
+        public byte[] BinaryResource { get; set; }
     }
 }
