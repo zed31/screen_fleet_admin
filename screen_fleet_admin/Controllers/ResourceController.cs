bd0e58518ba3febeeae81b9d6ceb5eff2d2c18c5
Zed <clement.francois@epitech.eu>
2018-05-06 12:36:18 +0200

    Controller: ADD: Resource controller

---
+++ screen_fleet_admin/Controllers/ResourceController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using screen_fleet_admin.Repositories;
+using screen_fleet_admin.Models;
+
+namespace screen_fleet_admin.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class ResourceController : Controller
+    {
+        private readonly IResourceRepository _repository;
+
+        public ResourceController(IResourceRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ResourceModel>> GetResources()
+        {
+            return await _repository.GetAllResources();
+        }
+    }
+}
