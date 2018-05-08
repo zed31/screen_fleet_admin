8fa3ed96f4666abd224d5432364f2285990ba51b
Zed <clement.francois@epitech.eu>
2018-05-08 17:26:13 +0200

    ResourceController: ADD: Remove, Edit and Insert

--- screen_fleet_admin/Controllers/ResourceController.cs
+++ screen_fleet_admin/Controllers/ResourceController.cs
@@ -25,10 +25,51 @@ namespace screen_fleet_admin.Controllers
         /*! \brief Get all the resources registered inside the composition
          * @return  An asynchronous task containing an enumerable container of resource model
          */
-        [HttpGet]
+        [HttpGet("list")]
         public async Task<IEnumerable<ResourceModel>> GetResources()
         {
             return await _repository.GetAllResources();
+        }
+
+        /*! \brief Get a specific resource related to the `RawId`
+         * @param[in]   RawId   The RawId field of the specific resource
+         * @return      an asynchronous task containing a ResourceModel object, or Nothing if no object
+         *              is found
+         */
+        [HttpGet("list/{RawId}")]
+        public async Task<ResourceModel> GetSpecificResource(string RawId)
+        {
+            return await _repository.GetSpecificResource(RawId);
+        }
+
+        /*! \brief Add Specific resource into the Resource Collection
+         * @param[in]   resource    The resource retrieved from the body of the post request
+         * @return      an asynchronous task containing only an Http code
+         */
+        [HttpPost("insert")]
+        public async Task Insert([FromBody] ResourceModel resource)
+        {
+            await _repository.AddNewResource(RepositoryUtils.GenerateNewModel(resource));
+        }
+
+        /*! \brief Edit a specific resource from the MongoDB database
+         * @param[in]   resource    the new content of the resource retrieved from the request body
+         * @return      an asynchronous task set to true if the edit succeed, false otherwise
+         */
+        [HttpPut("modify")]
+        public async Task<bool> Edit([FromBody] ResourceModel resource)
+        {
+            return await _repository.UpdateResource(resource.RawId, RepositoryUtils.GenerateNewModel(resource));
+        }
+
+        /*! \brief Remove a specific resource containing the `RawId`
+         * @param[in]   rawId   The id of the resource
+         * @return      an asynchronous task that returns true if the deletion succeed, false otherwise
+         */
+        [HttpDelete("remove/{RawId}")]
+        public async Task<bool> Remove(string rawId)
+        {
+            return await _repository.RemoveSpecificResource(rawId);
         }
     }
 }
