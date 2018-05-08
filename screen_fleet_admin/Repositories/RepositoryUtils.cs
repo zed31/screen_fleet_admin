b22f55f22554bef0345b13ee656476707b50611f
Zed <clement.francois@epitech.eu>
2018-05-08 17:28:26 +0200

    RepositoryUtils: ADD: Model generator and deep-copy

--- screen_fleet_admin/Repositories/RepositoryUtils.cs
+++ screen_fleet_admin/Repositories/RepositoryUtils.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson;
+using screen_fleet_admin.Models;
 
 namespace screen_fleet_admin.Repositories
 {
@@ -16,6 +17,44 @@ namespace screen_fleet_admin.Repositories
                 internalId = ObjectId.Empty;
 
             return internalId;
+        }
+
+        /*! \brief Generate new model according to the specific given model
+         * It removes only the ObjectID mongoDB to generate a new one
+         * @param[in]   model   the model deeply copied
+         * @return      A deep copy of the `model` parameter
+         */
+        public static ResourceModel GenerateNewModel(ResourceModel model)
+        {
+            return new ResourceModel()
+            {
+                Name = model.Name,
+                ResourceType = model.ResourceType,
+                RawId = model.RawId,
+                Leaf1 = model.Leaf1,
+                Leaf2 = model.Leaf2,
+                Path = model.Path,
+                InsertionDate = model.InsertionDate,
+                UpdateTime = model.UpdateTime
+            };
+        }
+
+        /*! \brief Generate new TVModel by copying the `model` parameter. It removes the ObjectId
+         * coming from MongoDB for convenience
+         * @param[in]   model   the model deeply copied
+         * @return      A new deep copy of the `model` parameter
+         */
+        public static TVModel GenerateNewModel(TVModel model)
+        {
+            return new TVModel()
+            {
+                Name = model.Name,
+                RawId = model.RawId,
+                Ip = model.Ip,
+                InsertionDate = model.InsertionDate,
+                UpdateTime = model.UpdateTime,
+                Resource = model.Resource
+            };
         }
     }
 }
